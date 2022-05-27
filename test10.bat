f776b719b27c44c70f5561d92b77659a8c7472e2
reverse-shell-host <106280975+reverse-shell-host@users.noreply.github.com>
2022-05-27 01:43:25 +0000

    Update and rename test9.bat to test10.bat

---
+++ test10.bat
@@ -0,0 +1,6 @@
+echo test
+echo $%username%
+echo $username
+set usrnm = %username%
+echo %usrnm%
+cd C:\users\%usrnm%
--- test9.bat
+++
@@ -1,4 +0,0 @@
-echo test
-echo %username%
-echo $username
-cd C:\users\%username%
